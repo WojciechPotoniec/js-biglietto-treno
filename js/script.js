3b78836dead372d8f88a209afd819eec6a6b7bf9
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-03-06 18:44:47 +0100

    update

--- js/script.js
+++ js/script.js
@@ -11,12 +11,33 @@ L'output del prezzo finale va messo fuori in forma umana
 */
 
 
-let userkilometres = parseInt(prompt('Inserisci i chilometri percorsi'));
-let userage = parseInt(prompt('Inserisci la tua età'));
-let young = (userage < 18);
-let old = (userage >= 65); 
-const ticketprice = (0.21 * userkilometres);
-const discountyoung = (20 / 100);
-const discountold = (40 / 100);
+let userkilometres = (prompt('Inserisci i chilometri percorsi')); //km scelti dall'user
+let userage = parseInt(prompt('Inserisci la tua età')); // età scelta dall'user
+let ticketprice = (0.21 * userkilometres); // Esempio: 0.21*10 = 2.10
 
-console.log(userkilometres, userage, young, old, discountyoung, discountold);
+
+if (isNaN(userage) && isNaN(userkilometres)) // mi darà true se userage non è un numero(NotaNumber)
+
+{
+    console.log('errore!! Devi inserire un numero.');
+    document.getElementById('finalPrice').innerHTML = `errore!! Devi inserire un numero.`
+}
+else{
+    if (userage < 18) {
+        //se l'utente è < di anni 18 allora prezzoBase = prezzoBase - ('SCONTO')
+        ticketprice = ticketprice - (ticketprice * 20 / 100); 
+        console.log(`Il cliente ha diritto ad uno sconto del 20%. Quindi il costo del biglietto è: ${ticketprice.toFixed(2)}&euro;`);
+        document.getElementById('finalPrice').innerHTML = `Il costo del biglietto è:  ${ticketprice.toFixed(2)}&euro;`
+    }
+    else if (userage >= 65) {
+        //se l'utente è >= di anni 65 allora prezzoBase = prezzoBase - ('SCONTO')
+        ticketprice = ticketprice - (ticketprice * 40 / 100); 
+        console.log(`Il cliente ha diritto ad uno sconto del 40%. Quindi il costo del biglietto è:  ${ticketprice.toFixed(2)}&euro;`);
+        document.getElementById('finalPrice').innerHTML = `Il costo del biglietto è:  ${ticketprice.toFixed(2)}&euro;`
+    }
+    else {
+        //se l'utente ha tra i 18 e i 65 anni allora paga per intero
+        console.log(`Il cliente paga il biglietto per intero. Quindi il prezzo è: ${ticketprice.toFixed(2)}&euro;`);
+        document.getElementById('finalPrice').innerHTML = `Il costo del biglietto è:  ${ticketprice.toFixed(2)}&euro;`
+    }
+}
